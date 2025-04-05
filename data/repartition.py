7b78c6f2875163c8505affba1b06c568364594c8
Tarek Allam Jr <t.allam.jr@gmail.com>
2025-04-05 12:34:18 +0100

    wip. update repartitioning

--- data/repartition.py
+++ data/repartition.py
@@ -1,26 +1,54 @@
+from pprint import pprint as print
+
 import polars as pl
+from polars.datatypes import *
 from polars.io.partition import PartitionByKey
 
-df = pl.scan_parquet("./yellow/*", allow_missing_columns=True)
+schema = {
+    "VendorID": Int64,
+    "tpep_pickup_datetime": Datetime(time_unit="ns", time_zone=None),
+    "tpep_dropoff_datetime": Datetime(time_unit="ns", time_zone=None),
+    "passenger_count": Int64,
+    "trip_distance": Float64,
+    "RatecodeID": Int64,
+    "store_and_fwd_flag": String,
+    "PULocationID": Int64,
+    "DOLocationID": Int64,
+    "payment_type": Int64,
+    "fare_amount": Float64,
+    "extra": Float64,
+    "mta_tax": Float64,
+    "tip_amount": Float64,
+    "tolls_amount": Float64,
+    "improvement_surcharge": Float64,
+    "total_amount": Float64,
+    "congestion_surcharge": Float64,
+    "airport_fee": Float64,
+}
 
-df = (
-    df.with_columns(
+
+for year in range(2018, 2019):
+    print(f"Filtering: {year}...")
+    df = pl.scan_parquet(f"./yellow/yellow_tripdata_{year}-0[3-6]*.parquet*")
+    df = df.with_columns(
         [
-            pl.col("Trip_Pickup_DateTime").str.strptime(
-                pl.Datetime, format="%Y-%m-%d %H:%M:%S"
-            ),
-            pl.col("Trip_Dropoff_DateTime").str.strptime(
-                pl.Datetime, format="%Y-%m-%d %H:%M:%S"
-            ),
+            pl.col("tpep_pickup_datetime").dt.year().alias("year"),
+            pl.col("tpep_pickup_datetime").dt.month().alias("month"),
+            # Explicitly cast columns per your schema:
+            pl.col("VendorID").cast(schema["VendorID"], strict=False),
+            pl.col("passenger_count").cast(schema["passenger_count"], strict=False),
+            pl.col("trip_distance").cast(schema["trip_distance"], strict=False),
+            pl.col("RatecodeID").cast(schema["RatecodeID"], strict=False),
+            # ... add additional casts as needed for each column ...
+            pl.col("congestion_surcharge")
+            .fill_null(0)
+            .cast(schema["congestion_surcharge"], strict=False),
+            pl.col("airport_fee")
+            .fill_null(0)
+            .cast(schema["airport_fee"], strict=False),
         ]
     )
-    .with_columns(
-        [
-            pl.col("Trip_Pickup_DateTime").dt.year().alias("year"),
-            pl.col("Trip_Pickup_DateTime").dt.month().alias("month"),
-        ]
-    )
-    .sink_parquet(
+    df.lazy().sink_parquet(
         PartitionByKey(
             "./yellow.hive/{key[0].name}={key[0].value}/{key[1].name}={key[1].value}/0000.parquet",
             by=[pl.col("year"), pl.col("month")],
@@ -29,4 +57,8 @@ df = (
         mkdir=True,
         engine="streaming",
     )
-)
+    print(f"Hive partition written to disk for: {year}...")
+
+df = pl.scan_parquet("yellow.hive/**/*.parquet", hive_partitioning=True)
+count = df.select(pl.len().count()).collect(engine="streaming")
+print(count)
